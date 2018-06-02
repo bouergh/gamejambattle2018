94499d3a2afb560383a6383f9f1cf5e7073c8832
bouergh <voodbutcher@gmail.com>
2018-06-02 03:34:35 -0400

    test invisi walls

---
+++ Assets/Scripts/KillObstacles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillObstacles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter(Collider other){
+		if(other.CompareTag("Obstacle")){
+				Destroy(other.gameObject);
+		}
+	}
+	
+}
