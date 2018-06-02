f9549f456bd68fea7a89717cbf1f15623f2dc14d
Math <mathieu.fontaine@outlook.com>
2018-06-02 16:55:42 -0400

    Metrique and Mass Fix

---
+++ Assets/Hands.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hands : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	 void OnCollisionEnter(Collision collision){
+
+		 foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+
+		 Debug.Log("colliding with stuff");
+		 if(collision.gameObject.CompareTag("Obstacle") && GetComponentInParent<PlayerController>().grabbing){
+			 GetComponentInParent<PlayerController>().Drag(collision.gameObject);
+			 Debug.Log("grabbed Obstacle");
+		 }
+	 }
+}
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hands : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	 void OnCollisionEnter(Collision collision){
+
+		 foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+
+		 Debug.Log("colliding with stuff");
+		 if(collision.gameObject.CompareTag("Obstacle") && GetComponentInParent<PlayerController>().grabbing){
+			 GetComponentInParent<PlayerController>().Drag(collision.gameObject);
+			 Debug.Log("grabbed Obstacle");
+		 }
+	 }
+}
+>>>>>>> 20c9192314dce194c8dbe7e618e786bee558dfe8
