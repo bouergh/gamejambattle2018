73e93a7288afb24e1d76181c2adaa0d60a3532b8
bouergh <voodbutcher@gmail.com>
2018-06-04 12:17:28 -0400

    character selection : setup scene ui + script objects, need to code now

---
+++ Assets/Scripts/CharacterSelector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelector : MonoBehaviour {
+
+
+	public PlayerController characterA;
+	public Image buttonA;
+	public PlayerController characterB;
+	public Image buttonB;
+	public PlayerController characterX;
+	public Image buttonX;
+	public PlayerController characterY;
+	public Image buttonY;
+
+
+	// Use this for initialization
+	void Start () {
+		//debug/editor instructions
+		Debug.Log("Press A to player "+characterA.gameObject.name);
+		Debug.Log("Press B to player "+characterB.gameObject.name);
+		Debug.Log("Press X to player "+characterX.gameObject.name);
+		Debug.Log("Press Y to player "+characterY.gameObject.name);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
